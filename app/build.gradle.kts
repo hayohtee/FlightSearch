c7bc0cda41c2b0e02f54b339f4ee4958b1fb4dd8
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-10-20 19:57:10 +0100

    Add room dependencies to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,11 @@ android {
 
 dependencies {
 
+    // Room dependency
+    implementation("androidx.room:room-runtime:2.6.0")
+    ksp("androidx.room:room-compiler:2.6.0")
+    implementation("androidx.room:room-ktx:2.6.0")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
